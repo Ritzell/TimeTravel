33a6066fe7ff59e8c2894cdddd2311fd13f387b4
Yuji Kawashima <kawasimayuji@gmail.com>
2016-12-06 21:49:10 +0900

    EntranceのリフレクションProbeの更新頻度を下げることでフレームレート平均400から平均750まで上昇。 StageManagerのステージ生成の距離と、後方ステージの削除が正常に行われていなかった問題を修正。

--- TimeTravelVR/Assets/StageGenerator.cs
+++ TimeTravelVR/Assets/StageGenerator.cs
@@ -11,6 +11,7 @@ public class StageGenerator : MonoBehaviour {
 	private GameObject player;
 
 	private int nowStage = 0;
+	private int minStage = 0;
 
 	void Start(){
 		player = FindObjectOfType<Player> ().gameObject;
@@ -18,15 +19,16 @@ public class StageGenerator : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float RearDistance = nowStage - GeneratorRange >= 0 ? Mathf.Abs(Vector3.Distance (Stages [nowStage - 2].transform.position, player.transform.position)) : 0;
-		float ForwardDistance = nowStage + GeneratorRange <= Stages.Length - 1 ? Mathf.Abs (Vector3.Distance (Stages [nowStage + 2].transform.position, player.transform.position)) : Mathf.Infinity;
+		float RearDistance = minStage <= Stages.Length - 1 ? Mathf.Abs(Vector3.Distance (Stages [minStage].transform.position, player.transform.position)) : 0;
+		float ForwardDistance = nowStage + GeneratorRange <= Stages.Length - 1 ? Mathf.Abs (Vector3.Distance (Stages [nowStage + GeneratorRange].transform.position, player.transform.position)) : Mathf.Infinity;
 
 		if (ForwardDistance < LoadDistance) {
 			Stages [nowStage+GeneratorRange].SetActive (true);
 			nowStage++;
 		}
 		if (RearDistance > LoadDistance) {
-			Stages [nowStage-GeneratorRange].SetActive (false);
+			Stages [minStage].SetActive (false);
+			minStage++;
 		}
 	}
 }
