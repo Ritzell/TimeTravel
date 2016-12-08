422d4c02a5d5533b9de3448d36df6925e486bcf3
河島有志 <kawasimayuji@gmail.com>
2016-12-08 12:45:45 +0900

    アイテムの挙動制御を仮実装。 天国の魂のランダム生成

--- TimeTravelVR/Assets/Scripts/Item.cs
+++ TimeTravelVR/Assets/Scripts/Item.cs
@@ -3,10 +3,24 @@ using System.Collections;
 
 public enum ItemType{
 	carry,
-	talk
+	talk,
+	controll
 }
 
 public class Item : MonoBehaviour {
+	[SerializeField]
+	private bool LockPositionX = false;
+	[SerializeField]
+	private bool LockPositionY = false;
+	[SerializeField]
+	private bool LockPositionZ = false;
+	[SerializeField]
+	private bool LockRotateX = false;
+	[SerializeField]
+	private bool LockRotateY = false;
+	[SerializeField]
+	private bool LockRotateZ = false;
+
 	public ItemType type;
 
 	public Vector3 CarryPosition;
@@ -28,10 +42,30 @@ public class Item : MonoBehaviour {
 			transform.parent = ob.transform;
 			transform.localPosition = CarryPosition;
 			break;
-		
+		case ItemType.controll:
+			transform.parent = ob.transform;
+			break;
 		default :
 			break;
 		}
+	}
+
+	public void Move(Vector3 target){
+		//座標を保存
+		Vector3 origin = transform.position;
+		transform.Translate (origin.x-target.x,origin.y - target.y,origin.z - target.z);
+		transform.position = new Vector3 (	LockPositionX ? origin.x : transform.position.x,
+											LockPositionY  ? origin.y : transform.position.y,
+											LockPositionZ ? origin.z : transform.position.z);
+	}
+
+	public void Rotate(Transform target){
+		//回転角度を保存
+		Vector3 origin = transform.eulerAngles;
+		transform.LookAt (target);
+		transform.eulerAngles = new Vector3 (	LockRotateX ? origin.x : transform.eulerAngles.x ,
+												LockRotateY ? origin.y : transform.eulerAngles.y,
+												LockRotateZ ? origin.z : transform.eulerAngles.z);
 	}
 
 	void OnDestroy(){
---
+++ TimeTravelVR/Assets/SoulGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoulGenerator : MonoBehaviour {
+	[SerializeField]
+	private int SpawnCount;
+	[SerializeField]
+	private GameObject soul;
+	// Use this for initialization
+	void Start () {
+		for (int count = 0; count < SpawnCount; count++) {
+			var InstantSoul = (GameObject)Instantiate(soul,new Vector3(Random.Range(-100,100),Random.Range(-100,100),Random.Range(-100,100)),Quaternion.identity);
+			InstantSoul.transform.parent = gameObject.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
